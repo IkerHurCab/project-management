4b982342252a68479671b2e7e89da58910260429
lucasmaragu <lucasmaragu@gmail.com>
2025-02-03 11:52:30 +0100

    feat: task & project models created

---
+++ app/Models/ProjectsManagement/Task.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $table = 'task';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'assigned_hours',
+        'start_date',
+        'end_date',
+        'status',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'project_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
