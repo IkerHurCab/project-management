792a56bb71627cf03d13b47978107593f1aedab0
lucasmaragu <lucasmaragu@gmail.com>
2025-02-03 11:49:00 +0100

    db: task & project tables created

---
+++ database/migrations/2025_02_03_103142_create_task_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->float('estimated_hours');
+            $table->float('completed_hours')->nullable();
+            $table->integer('priority');
+            $table->string('status');
+            $table->foreignId('project_id')->constrained('project')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
