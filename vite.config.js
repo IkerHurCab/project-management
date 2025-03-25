8d75281de7bace94fc14da000e987f22df72fae3
IkerHurCab <153614364+IkerHurCab@users.noreply.github.com>
2025-03-25 09:33:00 +0100

    Chore: protected Vite Config Info

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,3 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-// import tailwindcss from '@tailwindcss/vite';
-// import vue from '@vitejs/plugin-vue';
-
-// export default defineConfig({
-//     plugins: [
-//         tailwindcss(),
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//         vue({ 
-//             template: {
-//                 transformAssetUrls: {
-//                     base: null,
-//                     includeAbsolute: false,
-//                 },
-//                 compilerOptions: {
-//                     isCustomElement: (tag) => ['box-icon'].includes(tag),
-//                 }
-//             },
-//         }),
-//     ],
-
-//     resolve: { 
-//         alias: {
-//             vue: 'vue/dist/vue.esm-bundler.js',
-//         },
-//     },
-
-//     server: {
-//         // host: process.env.VITE_HOST || "0.0.0.0",
-//         // port: process.env.VITE_PORT,
-//         // hmr: {
-//         //     host: process.env.VITE_HMR_HOST || "localhost",
-//         //     protocol: process.env.VITE_HMR_PROTOCOL || "ws",
-//         // },
-//         cors: {
-//             origin: process.env.VITE_CORS_ORIGIN || '*',
-//         },
-//     },
-// });
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
@@ -74,7 +30,7 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 5184,
         hmr: {
-            host: "10.40.1.54",
+            host: "0.0.0.0",
             protocol: "ws",
         },
         cors: {
